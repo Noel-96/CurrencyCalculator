6999c32cf45946ab45b5ce88ce1246c23d50c5d3
Noel-96 <noelorion69@gmail.com>
2019-07-29 21:55:19 +0200

    Initial Commit

---
+++ CowryWiseInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CowryWiseInterview
+//
+//  Created by Noel Obaseki on 29/07/2019.
+//  Copyright © 2019 Tizeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
